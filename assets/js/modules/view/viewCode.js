f0637a9141cd074081b16062bb76ca7488cbb26d
dkanders12 <dkanders12@gmail.com>
2023-10-09 08:18:09 +0200

    t

--- assets/js/modules/view/viewCode.js
+++ assets/js/modules/view/viewCode.js
@@ -11,25 +11,62 @@ stock: 32
 thumbnail: "https://i.dummyjson.com/data/products/5/thumbnail.jpg"
 title: "Huawei P30" 
 */
-let categoriesDisplayElement = "categoriesNav";
-let productDisplayElement = "productDisplay";
+
 let productInfoElement = "productInfo";
+let shoppingListElement = document.getElementById("shoppingList"); // Hent shoppinglisten i HTML
+let toggleShoppingListButton = document.getElementById("toggleShoppingList"); // Hent knappen i HTML
 
-let categoriesNav = document.getElementById(categoriesDisplayElement);
+let clickedProducts = [];
 
+// Funktion til at vise eller skjule shoppinglisten
+function toggleShoppingList() {
+  if (
+    shoppingListElement.style.display === "none" ||
+    shoppingListElement.style.display === ""
+  ) {
+    shoppingListElement.style.display = "block"; // Vis shoppinglisten
+    toggleShoppingListButton.textContent = "Hide Shopping List"; // Opdater knaptekst
+  } else {
+    shoppingListElement.style.display = "none"; // Skjul shoppinglisten
+    toggleShoppingListButton.textContent = "Show Shopping List"; // Opdater knaptekst
+  }
+}
+
+// Tilknyt klikhåndtereren til knappen
+toggleShoppingListButton.addEventListener("click", toggleShoppingList);
+
+// Funktion til at vise produkter og tilføje dem til shoppinglisten
 export function displayProductInfo(products) {
   let productInfo = document.getElementById(productInfoElement);
   productInfo.innerHTML = ""; // Clear previous content
 
   products.forEach((product) => {
     let productItem = document.createElement("div");
-    productItem.textContent = `Product: ${product.title}, image s Price: ${product.price}`;
+    productItem.style.border = "1px solid black";
+    productItem.style.padding = "20px";
+    productItem.textContent = `Product: ${product.title}, Price: ${product.price}`;
     productInfo.appendChild(productItem);
+
     let productImage = document.createElement("img");
     productImage.src = product.thumbnail;
     productImage.alt = "Product Image";
+    productImage.style.width = "220px";
 
     // Append the image element to the productItem
     productItem.appendChild(productImage);
+
+    productItem.addEventListener("click", () => {
+      // Log the product information to the console
+      console.log(product.title);
+
+      // Tilføj det klikkede produkt til arrayet
+      clickedProducts.push(product);
+      console.log(clickedProducts);
+
+      // Opret et nyt listeitem for hvert produkt og tilføj det til shoppinglisten
+      let shoppingItem = document.createElement("li");
+      shoppingItem.textContent = `Product: ${product.title}, Price: ${product.price}`;
+      shoppingListElement.appendChild(shoppingItem);
+    });
   });
 }
